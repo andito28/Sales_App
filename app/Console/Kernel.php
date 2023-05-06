cf56761710df065959a7c4d7fd45c60d543b01fb
andito28 <andito763@gmail.com>
2023-05-06 22:44:43 +0800

    update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        \App\Console\Commands\CheckReminder::class,
+
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +20,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('check:reminder')->everyMinute(1);
+        // $schedule->command('check:reminder')->everyMinute();
     }
 
     /**
